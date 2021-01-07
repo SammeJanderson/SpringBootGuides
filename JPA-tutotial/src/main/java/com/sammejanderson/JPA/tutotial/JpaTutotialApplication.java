cff3ef6adbbd317706cc843bf3b11af53626ee40
Samme Janderson <samme.san@gmail.com>
2021-01-07 12:15:00 -0300

    JPA Spring guide setup

---
+++ JPA-tutotial/src/main/java/com/sammejanderson/JPA/tutotial/JpaTutotialApplication.java
@@ -0,0 +1,13 @@
+package com.sammejanderson.JPA.tutotial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaTutotialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaTutotialApplication.class, args);
+	}
+
+}
