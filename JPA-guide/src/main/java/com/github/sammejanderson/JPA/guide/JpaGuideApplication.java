61bc8665b6a8457565288e5c59d8655666ecef84
Samme Janderson <samme.san@gmail.com>
2021-01-07 12:26:35 -0300

    JPA guide setup

---
+++ JPA-guide/src/main/java/com/github/sammejanderson/JPA/guide/JpaGuideApplication.java
@@ -0,0 +1,13 @@
+package com.github.sammejanderson.JPA.guide;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaGuideApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaGuideApplication.class, args);
+	}
+
+}
